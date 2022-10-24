8e82bedd2eced03dd574921a69b0177b985a027f
ChanakaDev <mr.mbcnb@gmail.com>
2022-10-25 01:17:59 +0530

    WTA-02 - First screen is completed

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: avoid_print
+
 import 'package:flutter/material.dart';
 
 void main() {
@@ -10,57 +12,202 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const HomeScreen(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  final String title;
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({super.key});
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  State<HomeScreen> createState() => _HomeScreenState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
+class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
+      body: Column(
+        children: [
+          // App name row
+          Container(
+            padding: const EdgeInsets.only(
+                left: 32.0, right: 32.0, top: 32.0, bottom: 16.0),
+            child: Image.asset('assets/images/title_area@2x.png'),
+          ),
+          // Image row
+          Container(
+            decoration: BoxDecoration(
+              color: Colors.white,
+              borderRadius: const BorderRadius.only(
+                topLeft: Radius.circular(10),
+                topRight: Radius.circular(10),
+                bottomLeft: Radius.circular(10),
+                bottomRight: Radius.circular(10),
+              ),
+              boxShadow: [
+                BoxShadow(
+                  color: Colors.grey.withOpacity(0.5),
+                  spreadRadius: 5,
+                  blurRadius: 7,
+                  offset: const Offset(0, 3), // changes position of shadow
+                ),
+              ],
+            ),
+            margin: const EdgeInsets.symmetric(horizontal: 16.0),
+            child: ClipRRect(
+              borderRadius: BorderRadius.circular(15.0),
+              child: Image.asset('assets/images/image@2x.png'),
+            ),
+          ),
+          const SizedBox(
+            height: 8,
+          ),
+          // Parameters row
+          parameterRowBuilder(
+            "assets/images/icon_1@2x.png",
+            "Water capacity",
+          ),
+          parameterRowBuilder(
+            "assets/images/icon_2@2x.png",
+            "Temperature",
+          ),
+          parameterRowBuilder(
+            "assets/images/icon_3@2x.png",
+            "Clearity",
+          ),
+          parameterRowBuilder(
+            "assets/images/icon_4@2x.png",
+            "Speed",
+          ),
+          parameterRowBuilder(
+            "assets/images/icon_5@2x.png",
+            "Daily consumption",
+          ),
+          const SizedBox(
+            height: 8,
+          ),
+          // Water pump button row
+          waterPumpButtonBuilder(),
+          // Bottom navigation button row
+          const SizedBox(
+            height: 30,
+          ),
+          Expanded(
+            child: Container(
+              height: 80,
+              color: const Color(0xff383F85),
+              child: Row(
+                // ignore: prefer_const_literals_to_create_immutables
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  const Text(
+                    "Usage per hour",
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 16.0,
+                    ),
+                  ),
+                  Container(
+                    margin: const EdgeInsets.only(left: 8),
+                    width: 25,
+                    child: Image.asset('assets/images/history@2x.png'),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ],
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+    );
+  }
+
+  InkWell waterPumpButtonBuilder() {
+    return InkWell(
+      onTap: () {
+        print("Button clicked");
+      },
+      child: Container(
+        height: 50,
+        width: double.infinity,
+        decoration: BoxDecoration(
+          color: const Color(0xff435E82),
+          border: Border.all(
+            color: const Color(0xff435E82),
+          ),
+          borderRadius: const BorderRadius.all(
+            Radius.circular(20),
+          ),
+        ),
+        margin: const EdgeInsets.only(
+          left: 16.0,
+          right: 16.0,
+          top: 8.0,
+        ),
+        // color: const Color(0xff435E82),
+        child: const Center(
+          child: Text(
+            "Water pump (Turn ON)",
+            style: TextStyle(
+              color: Colors.white,
+              fontSize: 16.0,
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
+          ),
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
+    );
+  }
+
+  Padding parameterRowBuilder(
+    String imagePath,
+    String parameterName,
+  ) {
+    return Padding(
+      padding: const EdgeInsets.only(top: 8.0),
+      child: Column(
+        children: [
+          Container(
+            margin: const EdgeInsets.symmetric(horizontal: 16),
+            height: 50,
+            decoration: BoxDecoration(
+              border: Border.all(
+                color: const Color(0xff389CD7),
+                width: 2,
+              ),
+              borderRadius: const BorderRadius.all(
+                Radius.circular(20),
+              ),
+            ),
+            child: Row(
+              // ignore: prefer_const_literals_to_create_immutables
+              children: [
+                Container(
+                  padding: const EdgeInsets.only(
+                    left: 16.0,
+                    right: 8.0,
+                  ),
+                  child: SizedBox(
+                    height: 30,
+                    child: Image.asset(imagePath),
+                  ),
+                ),
+                Text(
+                  parameterName,
+                  style: const TextStyle(
+                    color: Color(0xff383F85),
+                    fontSize: 16,
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
       ),
     );
   }
